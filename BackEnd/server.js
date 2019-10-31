eacba55082763b6419960f8c3e894d2358c493cf
Martin Kenirons <martin.kenirons@gmit.ie>
2019-10-31 14:35:39 +0000

    lab_upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,6 +4,11 @@ const port = 4000
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const mongodDB = 'mongodb+srv://admin:admin@cluster0-lk0ot.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongodDB,{useNewUrlParser:true});
+
 app.use(cors());
 
 app.use(function (req, res, next) {
@@ -19,6 +24,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title:String,
+    year:String,
+    poster:String
+});
+
+const MovieModel = mongoose.model('movies', movieSchema);
+
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
@@ -46,32 +62,47 @@ app.get('/test', (req, res) => {
 
 app.post('/api/movies', (req,res) =>{
     console.log(req.body);
-    console.log(req.body.title);
-    console.log(req.body.year);
-    console.log(req.body.poster);
+
+    MovieModel.create({
+        title:req.body.title,
+        year:req.body.year,
+        poster:req.body.poster
+    });
+
     res.json('Data Uploaded');
+})
+
+app.get('/api/movies/:id', (req, res) =>{
+    console.log(req.params.id);
+
+    MovieModel.findById(req.params.id, (error, data) =>{
+        res.json(data);
+    })
 })
 
 app.get('/api/movies', (req, res) => {
 
-    const myMovies = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        }
-    ];
+    MovieModel.find((error, data)=>{
+        res.json({movies:data});
+})
+    // const myMovies = [
+    //     {
+    //         "Title": "Avengers: Infinity War",
+    //         "Year": "2018",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    //     },
+    //     {
+    //         "Title": "Captain America: Civil War",
+    //         "Year": "2016",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    //     }
+    // ];
 
-    res.status(200).json(
-        {
-            movies: myMovies,
-            message: 'Data Sent'
-        });
+    // res.status(200).json(
+    //     {
+    //         movies: myMovies,
+    //         message: 'Data Sent'
+    //     });
 })
 
 app.get('/hello/:name', (req, res) => {
--- src/components/movies.js
+++ src/components/movies.js
@@ -6,7 +6,7 @@ class Movies extends React.Component{
     render(){
         return this.props.myMovies.map((movie)=>{
             //console.log({movie});
-            return <MovieItem key={movie.Poster} movie={movie}></MovieItem>
+            return <MovieItem key={movie.poster} movie={movie}></MovieItem>
         });
     }
 }
